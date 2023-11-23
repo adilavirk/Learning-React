c86f8b3b08d400f990b6a8e524d63be1e6971927
adilavirk <adilavirk0@gmail.com>
2023-11-23 13:06:16 +0500

    "completed todo project using redux  toolkit for state management."

---
+++ 10todocontextlocalstorage/src/App.jsx
@@ -0,0 +1,14 @@
+import './App.css'
+
+function App() {
+
+
+  return (
+    <>
+      <h1 className='bg-slate-600'>happy srfcoding!</h1>
+
+    </>
+  )
+}
+
+export default App
---
+++ reduxToolkitTodo/src/App.jsx
@@ -0,0 +1,23 @@
+
+import './App.css'
+import Todos from './components/Todos'
+import AddTodo from './components/addTodo'
+
+function App() {
+
+
+  return (
+
+    <>
+      <h1 className='text-2xl text-white font-medium'>Learn About Redux Toolkit</h1>
+      <AddTodo />
+      <Todos />
+
+
+
+    </>
+
+  )
+}
+
+export default App
