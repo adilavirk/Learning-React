f6638bc0b5d16fced27d1320e4a643e5ebac9f72
adilavirk <adilavirk0@gmail.com>
2023-12-16 09:53:21 +0500

    modified:   12MegaBlogWithAppwrite/src/App.jsx new file:   12MegaBlogWithAppwrite/src/appwrite/config.js new file:   12MegaBlogWithAppwrite/src/components/AuthLayout.jsx new file:   12MegaBlogWithAppwrite/src/components/Button.jsx new file:   12MegaBlogWithAppwrite/src/components/Footer/Footer.jsx new file:   12MegaBlogWithAppwrite/src/components/Header/Header.jsx new file:   12MegaBlogWithAppwrite/src/components/Header/LogoutBtn.jsx new file:   12MegaBlogWithAppwrite/src/components/Input.jsx new file:   12MegaBlogWithAppwrite/src/components/Login.jsx new file:   12MegaBlogWithAppwrite/src/components/Logo.jsx new file:   12MegaBlogWithAppwrite/src/components/PostCard.jsx new file:   12MegaBlogWithAppwrite/src/components/RTE.jsx new file:   12MegaBlogWithAppwrite/src/components/Select.jsx new file:   12MegaBlogWithAppwrite/src/components/Signup.jsx new file:   12MegaBlogWithAppwrite/src/components/container/Container.jsx new file:   12MegaBlogWithAppwrite/src/components/index.js new file:   12MegaBlogWithAppwrite/src/components/post-form/PostForm.jsx modified:   12MegaBlogWithAppwrite/src/main.jsx new file:   12MegaBlogWithAppwrite/src/store/authSlice.js new file:   12MegaBlogWithAppwrite/src/store/store.js

--- 12MegaBlogWithAppwrite/src/App.jsx
+++ 12MegaBlogWithAppwrite/src/App.jsx
@@ -1,13 +1,39 @@
-import './App.css'
+import { useEffect, useState } from "react"
+import { useDispatch } from 'react-redux'
+import authService from './appwrite/auth'
+import { login, logout } from "./store/authSlice"
+import { Header, Footer } from "./components"
+
 
 function App() {
 
+  const [loading, setLoading] = useState(true)
+  const dispatch = useDispatch()
 
-  return (
-    <>
-      <h1 className='bg-indigo-800'>React Mega Project</h1>
-    </>
-  )
+  useEffect(() => {
+    authService.getCurrentUser()
+      .then((userData) => {
+        if (userData) {
+          dispatch(login({ userData }))
+        } else {
+          dispatch(logout())
+        }
+      })
+      .finally(() => setLoading(false))
+  }, [])
+
+  return !loading ? (
+    <div className="min-h-screen flex flex-wrap content-between bg-gray-400">
+      <div className="w-full block">
+        <Header />
+        <main>
+          Todo: {/* <Outlet /> */}
+        </main>
+        <Footer />
+      </div>
+    </div>
+
+  ) : (null)
 }
 
 export default App
---
+++ 12MegaBlogWithAppwrite/src/components/index.js
@@ -0,0 +1,9 @@
+// file to export all the components from a single file
+import Header from "./Header/Header";
+import Footer from "./Footer/Footer";
+import Logo from "./Logo";
+import Container from "./container/Container";
+import LogoutBtn from "./Header/LogoutBtn";
+import RTE from "./RTE";
+import Select from "./Select";
+export { Header, Footer, Logo, Container, LogoutBtn, RTE, Select };
